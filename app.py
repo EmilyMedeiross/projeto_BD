890c381e78b536ff483748337256cd6a9d9a26ee
Emily Medeiros <161541242+EmilyMedeiross@users.noreply.github.com>
2024-10-20 19:46:39 -0300

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, redirect, render_template, url_for, request, flash
 from models import User, obter_conexao
 from werkzeug.security import check_password_hash, generate_password_hash
 from flask_login import LoginManager, login_user, login_required, logout_user
+#from flask_mysqldb import MySQL
 
 login_manager = LoginManager()
 app = Flask(__name__)
@@ -41,7 +42,7 @@ def login():
         if user and check_password_hash(user['senha'], senha):
             login_user(User.get(user['id']))
             flash("Você está logado")
-            return redirect(url_for('recomend'))
+            return redirect(url_for('criar_tarefa'))
         
         else:
             flash("Dados incorretos")
@@ -49,10 +50,11 @@ def login():
     return render_template('pages/login.html')
              
 
-@app.route('/criar_tarefa', methods=['GET', 'POST'])
+@app.route('/criar_tarefa', methods=['GET', 'POST']) 
 def criar_tarefa():
 
     if request.method == 'POST':
+
         nome = request.form['nome']
         descricao = request.form['descriçao']
         situacao = request.form['status']
@@ -61,21 +63,47 @@ def criar_tarefa():
         prioridade = request.form['prioridade']
         palavra_chave = request.form['palavra_chave']
         categoria = request.form['categoria']
-
-        conn = conexao.connection.cursor()
+        """ banco de dados diretamente no código -  conn = conexao.connection.cursor()
         conn.execute ("INSERT INTO tarefas(nome, descricao, situacao, data_criacao, prazo, prioridade, palavra_chave, categoria) VALUES(%s, %s, %s, %s, %s, %s, %s, %s)", (nome, descricao, situacao, data_criacao, prazo, prioridade, palavra_chave, categoria,))
         conexao.connection.commit()
-        coenxao.connection.close()
-    
+        conn.close()"""
+        tarefas = User(nome=nome, descricao=descricao, situacao=situacao, data_criacao=data_criacao, prazo=prazo, prioridade=prioridade, palavra_chave=palavra_chave, categoria=categoria, tarefas=tarefas)
+        tarefas.save_tarefas    
     return render_template("pages/criar_tarefa.html")
 
 @app.route('/atualizar_tarefa')
 def atualizar_tarefa():
+    
     return render_template("pages/atualizar_tarefa.html")
+
 
 @app.route('/listar_tarefa')
 def listar_tarefa():
-    return render_template("pages/listar_tarefa.html")
+    status = request.form['status']
+    tarefas = User(status=status)
+    tarefas.listar_tarefas
+
+    """  Banco de dados diretamente no código - conn = conexao.connection.cursor() # type: ignore
+    conn.execute("SELECT * FROM pecas")
+    tarefas = conn.fetchall()
+    conn.close()
+    return render_template('pages/listar_tarefa.html', tarefas=tarefas)"""
+
+
+@app.route('/<int:id>/deletar')
+def deletar():
+    if request.method == 'POST':
+      tarefa = request.form['tarefa']
+      tarefas = User(tarefa=tarefa)
+      tarefas.deletar_tarefas
+    return render_template("pages/deletar.html")
+    """ banco de dados diretamente no código -  conn = conexao.connection.cursor()
+    conn.execute("DELETE FROM pecas WHERE id = %s", (id,))
+    conexao.connection.commit()
+    conn.close()
+    return redirect(url_for('listar_pecas'))"""
+
+
 
 # 8 - logout
 @app.route('/logout')
