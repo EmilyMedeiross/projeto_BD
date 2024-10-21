9a26c7e2e5c3c6f894964ec79ab43da11ef17ac7
Mateus Lucena <150374285+Mathewxs21@users.noreply.github.com>
2024-10-21 07:36:30 -0300

    Update schema.sql

--- database/schema.sql
+++ database/schema.sql
@@ -1,13 +1,13 @@
 CREATE DATABASE IF NOT EXISTS `db_taferas`;
 USE `db_tarefas`;
 
-CREATE TABLE IF NOT EXISTS users (
+CREATE TABLE IF NOT EXISTS tb_users (
     use_id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
     use_email TEXT NOT NULL,
     use_senha TEXT NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS tarefas (
+CREATE TABLE IF NOT EXISTS tb_tarefas (
     tar_id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
     tar_nome TEXT NOT NULL,
     tar_descricao TEXT NOT NULL,
