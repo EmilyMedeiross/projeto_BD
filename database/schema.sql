6a7812ddf5da06518cd560ab8b4f0337a10e64b4
Mateus Lucena <150374285+Mathewxs21@users.noreply.github.com>
2024-10-22 05:28:29 -0300

    Update schema.sql

--- database/schema.sql
+++ database/schema.sql
@@ -17,6 +17,6 @@ CREATE TABLE IF NOT EXISTS tb_tarefas (
     tar_prioridade TEXT NOT NULL,
     tar_palavra_chave TEXT NOT NULL,
     tar_categoria TEXT NOT NULL
-    FOREIGN KEY (tar_user_id) REFERENCES tb_users(user_id)
+    FOREIGN KEY (tar_use_id) REFERENCES tb_users(use_id)
 );
 
