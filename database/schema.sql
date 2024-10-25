827d7b933d7a096e255f57370d281c14c42b6e08
Mickaelle <151803781+karinesouza06@users.noreply.github.com>
2024-10-25 19:38:22 -0300

    Update schema.sql

--- database/schema.sql
+++ database/schema.sql
@@ -13,10 +13,9 @@ CREATE TABLE IF NOT EXISTS tb_tarefas (
     tar_descricao TEXT NOT NULL,
     tar_situacao TEXT NOT NULL,
     tar_data_criacao DATE NOT NULL,
-    tar_prazo INT NOT NULL,
     tar_prioridade TEXT NOT NULL,
     tar_palavra_chave TEXT NOT NULL,
-    tar_categoria TEXT NOT NULL
+    tar_categoria TEXT NOT NULL,
+    tar_use_id INT NOT NULL, 
     FOREIGN KEY (tar_use_id) REFERENCES tb_users(use_id)
 );
-
